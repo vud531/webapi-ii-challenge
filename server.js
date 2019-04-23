c10edb3be24c1e6218155db5339e487a8adc21d6
Duc Vu <de.yd315@gmail.com>
2019-04-23 14:23:01 -0500

    successfully incorporated router into this api

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const postsRouter = require('./posts/posts-router.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Lambda Posts API</h>
+    <p>Welcome to the Lambda Posts API</p>
+  `);
+});
+
+server.use('/api/posts', postsRouter);
+
+// export default server
+module.exports = server; // <<<<<<<<<<<<<<<<<<<<<<<<<<  export the server
