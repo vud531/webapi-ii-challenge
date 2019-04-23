1f4bf59e188b8078cff2fb0afec754935e6b107d
Duc Vu <de.yd315@gmail.com>
2019-04-22 19:03:10 -0500

    got server to load

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
+const server = express();
 
-server.listen(4000, () => {
+server.use(express.json());
+
+server.listen(5000, () => {
     console.log('\n*** Server Running on http://localhost:4000 ***\n');
   });
