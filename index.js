01d22be1768c4db76f9a065e098b5cc7817b727e
Duc Vu <de.yd315@gmail.com>
2019-04-23 22:39:24 -0500

    configured the server, disabled put and delete operations

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config(); // add this line as the first thing to run1
+const port = process.env.PORT || 5000;
+
 // import server from './server.js'
 const server = require('./server.js'); // <<<<<<<<<< import server
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
--- posts/posts-router.js
+++ posts/posts-router.js
@@ -63,39 +63,39 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
-  try {
-    const count = await Posts.remove(req.params.id);
-    if (count > 0) {
-      res.status(200).json({ message: 'The post has been nuked' });
-    } else {
-      res.status(404).json({ message: 'The post could not be found' });
-    }
-  } catch (error) {
-    // log error to database
-    console.log(error);
-    res.status(500).json({
-      message: 'Error removing the post',
-    });
-  }
-});
+// router.delete('/:id', async (req, res) => {
+//   try {
+//     const count = await Posts.remove(req.params.id);
+//     if (count > 0) {
+//       res.status(200).json({ message: 'The post has been nuked' });
+//     } else {
+//       res.status(404).json({ message: 'The post could not be found' });
+//     }
+//   } catch (error) {
+//     // log error to database
+//     console.log(error);
+//     res.status(500).json({
+//       message: 'Error removing the post',
+//     });
+//   }
+// });
 
-router.put('/:id', async (req, res) => {
-  try {
-    const post = await Posts.update(req.params.id, req.body);
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: 'The post could not be found' });
-    }
-  } catch (error) {
-    // log error to database
-    console.log(error);
-    res.status(500).json({
-      message: 'Error updating the post',
-    });
-  }
-});
+// router.put('/:id', async (req, res) => {
+//   try {
+//     const post = await Posts.update(req.params.id, req.body);
+//     if (post) {
+//       res.status(200).json(post);
+//     } else {
+//       res.status(404).json({ message: 'The post could not be found' });
+//     }
+//   } catch (error) {
+//     // log error to database
+//     console.log(error);
+//     res.status(500).json({
+//       message: 'Error updating the post',
+//     });
+//   }
+// });
 
 
 // add an endpoint that returns all the messages for a post
